a94bf8bba01e1949b01219873ee767118d922bf5
Stefano Karapetsas <stefano@karapetsas.com>
2012-11-28 16:51:20 +0100

    start migration to MATE use mate-autogen instead of gnome-autogen.sh in autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -4,7 +4,7 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-PKG_NAME="slab"
+PKG_NAME="gnome-main-menu"
 
 (test -f $srcdir/configure.in \
   && test -f $srcdir/autogen.sh) || {
@@ -17,10 +17,10 @@ DIE=0
 
 rm -f .using-gnome-libs-package
 
-if ! which gnome-autogen.sh ; then
-  echo "You need to install the gnome-common module and make"
-  echo "sure the gnome-autogen.sh script is in your \$PATH."
+if ! which mate-autogen ; then
+  echo "You need to install the mate-common module and make"
+  echo "sure the mate-autogen script is in your \$PATH."
   exit 1
 fi
 
-REQUIRED_AUTOMAKE_VERSION=1.9 . gnome-autogen.sh
+REQUIRED_AUTOMAKE_VERSION=1.9 . mate-autogen
