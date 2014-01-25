0a4b7ca97a61a6cd46723969de432ee61c75b6f1
Stefano Karapetsas <stefano@karapetsas.com>
2014-01-25 18:53:55 +0100

    Update autogen.sh for configure.ac rename

--- autogen.sh
+++ autogen.sh
@@ -6,7 +6,7 @@ test -z "$srcdir" && srcdir=.
 
 PKG_NAME="gnome-main-menu"
 
-(test -f $srcdir/configure.in \
+(test -f $srcdir/configure.ac \
   && test -f $srcdir/autogen.sh) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
     echo " top-level $PKG_NAME directory"
