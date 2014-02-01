b7f5de4b793f7e71f9f6860046b3f03e8997ae33
Piotr Drąg <piotrdrag@gmail.com>
2014-02-01 19:58:38 +0100

    Update FSF address

--- main-menu/src/eggaccelerators.c
+++ main-menu/src/eggaccelerators.c
@@ -14,8 +14,7 @@
  * more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with libslab; if not, write to the Free Software Foundation, Inc., 51
- * Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * along with libslab; if not, see <http://www.gnu.org/licenses/>.
  */
 
 /* eggaccelerators.c
@@ -33,9 +32,7 @@
  * Library General Public License for more details.
  *
  * You should have received a copy of the GNU Library General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "eggaccelerators.h"
